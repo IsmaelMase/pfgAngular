96c8333427aa7a1d2cf6e4d45d9613488dcedd62
IsmaelMase <ismaelmfb@gmail.com>
2018-06-15 12:59:31 +0200

    commit

--- src/app/servicios/serviceConstants.ts
+++ src/app/servicios/serviceConstants.ts
@@ -1,5 +1,3 @@
 export var CONSTANTS = {
-	// url: 'https://salerev.herokuapp.com/api/'
-	// url: 'http://localhost:8080/api/'
-	url: 'http://167.99.141.41:8080/api/'
+	url: 'http://159.65.18.9:8080/api/'
 };
